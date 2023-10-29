bd11b50895fc7ca011e264f6ad3dd86065c40c34
choldi <jordias@gmail.com>
2023-10-29 21:50:00 +0100

    correct relation with session and result

--- app/models/search.py
+++ app/models/search.py
@@ -1,8 +1,12 @@
 from Model import db
+from sqlalchemy.sql import func
+
+MAX_QUERY=5
 
 class Search(db.Model):
     __tablename__ = 'search'
     id = db.Column(db.Integer, primary_key=True)
     session_id = db.Column(db.Integer, db.ForeignKey('session.id'), nullable=False)
+    search_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
     results = db.relationship('Result', backref='search', lazy=True)
 
