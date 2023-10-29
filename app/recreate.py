350f896bbbb7851ee5f2c68cb9d33754e70bb18d
choldi <jordias@gmail.com>
2023-10-29 21:48:59 +0100

    create default user

--- app/recreate.py
+++ app/recreate.py
@@ -1,7 +1,12 @@
 from Model import db
 from app import app
+from models.user import User
 
 with app.test_request_context():
 
      db.drop_all()
      db.create_all()
+     root=User(id=1,username='root',password='toor',email='root@mail.com')
+     db.session.add(root)
+     db.session.commit()
+     
