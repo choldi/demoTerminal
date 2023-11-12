78c542d62047711a11729768fd9a367df7ec4ec7
choldi <jordias@gmail.com>
2023-11-12 23:08:12 +0100

    create init method

--- app/models/link.py
+++ app/models/link.py
@@ -6,3 +6,7 @@ class Link(db.Model):
     added_id = db.Column(db.Integer, db.ForeignKey('added.id'), nullable=False)
     link = db.Column(db.String, unique=False,nullable=False)
 
+    def __init__(self,_link,_added_id):
+        self.link=_link
+        self.added_id=_added_id
+        
